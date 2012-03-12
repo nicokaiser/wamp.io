a4860d44327a259b38b2e2f2fe00af7f5f656702
Nico Kaiser <nico@siriux.net>
2012-03-12 17:38:42 +0100

    Add simple REST API for pubsub server

--- examples/pubsub/server.js
+++ examples/pubsub/server.js
@@ -3,8 +3,56 @@
  * Module dependencies.
  */
 
-var wsio = require('websocket.io')
+var http = require('http')
+  , qs = require('querystring')
+  , wsio = require('websocket.io')
   , wamp = require('../../lib/wamp.io');
+
+/**
+ * WebSocket server
+ */
 
 var ws = wsio.listen(9000);
 var app = wamp.attach(ws);
+
+/**
+ * REST API
+ */
+
+var api = http.createServer(function(req, res) {
+  if (req.url !== '/hub' ||  req.method !== 'POST') {
+    res.writeHead(400);
+    res.end();
+    return;
+  }
+
+  var buffer = '';
+
+  req.on('data', function(data) {
+    buffer += data;
+  });
+
+  req.on('end', function() {
+    try {
+      var parsed = qs.parse(buffer);
+    
+      var topicUri = parsed.topicuri;
+      var event = JSON.parse(parsed.body);
+
+      console.log('pushing ' + event + ' to ' + topicUri);
+      app.publish(topicUri, event);
+    } catch (e) {
+      res.writeHead(400);
+      res.end();
+      return;
+    }
+    res.writeHead(200);
+    res.end();
+  });
+
+  req.on('close', function() {
+    buffer = '';
+  });
+});
+
+api.listen(9090);
