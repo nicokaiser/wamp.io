889f7ef1a0d421f3651bc7d3b9801fa4f225c9c9
paulpdaniels <paulpdaniels@gmail.com>
2014-10-07 12:14:33 -0700

    Added error msg handling for the call results to the client. Refactored the response structure and the promises.  Updated the tests.

--- test/wamp.io.js
+++ test/wamp.io.js
@@ -1,48 +1,82 @@
 'use strict';
 
-var assert = require("assert");
-var should = require("should");
+//var should = require("should");
+var chai = require('chai');
+var should = chai.should();
+var chaiAsPromised = require('chai-as-promised');
 var sinon = require('sinon');
 var WebSocketServer = require('ws').Server;
 var wamp = require('../lib/wamp.io');
 
-describe('Array', function(){
-    describe('#indexOf()', function(){
-        it('should return -1 when the value is not present', function(){
-            assert.equal(-1, [1,2,3].indexOf(5));
-            assert.equal(-1, [1,2,3].indexOf(0));
-        })
-    })
-});
+chai.use(chaiAsPromised);
 
 describe('wamp', function(){
 
-    var wss;
 
-    beforeEach(function(){
-        wss = new WebSocketServer({port : 9001});
-    });
-
-    afterEach(function(){
-        wss.close();
-    });
 
     describe('#attach', function(){
 
+        var wss;
+
+        beforeEach(function(){
+            wss = new WebSocketServer({port : 9001});
+
+        });
+
+        afterEach(function(){
+            wss.close();
+        });
+
         it('should fail if the websocket is undefined', function(){
-            should(function(){wamp.attach(undefined);}).throw(Error);
+            (function(){wamp.attach(undefined)}).should.throw(Error);
+//            should(function(){wamp.attach(undefined);}).throw(Error);
         });
 
         it('should generate id on connection', function(done){
 
             var conn = wamp.attach(wss);
 
-            conn.on('connected', function(id) {
+            conn.on('clientconnected', function(id) {
                 id.should.be.ok;
                 done();
             });
 
             var client = wamp.connect("ws://localhost:9001");
         });
+    });
+
+    describe('#call', function() {
+
+        var wss;
+
+        before(function(){
+            wss = new WebSocketServer({port : 9001});
+
+
+
+            var server = wamp.attach(wss);
+
+            server.on('call', function(uri, args, cb){
+                if (uri === 'http://test.com/test#pass')
+                    cb(null, true);
+                else if (uri === 'http://test.com/test#fail')
+                    cb(new Error("failed!"), undefined);
+            });
+        });
+
+        it('should resolve functions that are completed', function(done) {
+
+
+            var client = wamp.connect("ws://localhost:9001", function(){
+                client.call('http://test.com/test#pass', false).should.eventually.equal(true).notify(done);
+            });
+        });
+
+        it('should reject functions that are errors', function(done) {
+            var client = wamp.connect("ws://localhost:9001", function(){
+                client.call('http://test.com/test#fail', false).should.eventually.be.rejected.notify(done);
+            });
+        })
+
     });
 });
